8f8580abd4b4b81fe84f6a734f335695caa3501d
chen1105695211 <1105695211@qq.com>
2016-05-27 15:33:42 +0800

    jiayajie

---
+++ app/src/main/java/com/caih/caih/MainActivity.java
@@ -0,0 +1,16 @@
+package com.caih.caih;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Administrator on 2016/5/27.
+ */
+public class MainActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
