3c575997f7928b2d4c7959524e97e316a11619d2
a <a>
2016-05-27 17:07:13 +0800

    李豆

--- app/src/main/java/com/caih/caih/StartActivity.java
+++ app/src/main/java/com/caih/caih/StartActivity.java
@@ -1,7 +1,11 @@
 package com.caih.caih;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        TextView text_ACCA=(TextView) findViewById(R.id.text_ACCA);
+        text_ACCA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(StartActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
