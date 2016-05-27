c3905043939fc16b6c39fe28dd8f3af617bde047
a <a>
2016-05-27 15:12:33 +0800

    李豆

--- app/src/main/java/com/caih/caih/SplashActivity.java
+++ app/src/main/java/com/caih/caih/SplashActivity.java
@@ -9,8 +9,6 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        //jfaighiewjriojwaeifkonvfjidjo
-        //kjm,k
-        //埃及发奖金家具佛加急费劲啊卡开了房间了看见看没看就打了付款了开始考虑分了吗开始
+
     }
 }
