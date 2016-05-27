7b1664f4370f903120c155f5684142499681401b
chen1105695211 <1105695211@qq.com>
2016-05-27 16:38:48 +0800

    jiayajie

--- app/src/main/java/com/caih/caih/MainActivity.java
+++ app/src/main/java/com/caih/caih/MainActivity.java
@@ -3,6 +3,10 @@ package com.caih.caih;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.caih.fragment.Kczx_Frag;
 
 /**
  * Created by Administrator on 2016/5/27.
@@ -11,6 +15,12 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.main_frag,new Kczx_Frag());
+        transaction.commit();
 
     }
 }
