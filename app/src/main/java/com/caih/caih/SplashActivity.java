3fb96573c4de001878fc41b8dee6b47447087347
Chen1105695211 <1105695211@qq.com>
2016-05-27 17:52:02 +0800

    chenpan

--- app/src/main/java/com/caih/caih/SplashActivity.java
+++ app/src/main/java/com/caih/caih/SplashActivity.java
@@ -1,14 +1,45 @@
 package com.caih.caih;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 import com.zhy.autolayout.AutoLayoutActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+
 public class SplashActivity extends AutoLayoutActivity {
+
+    Handler handler=new Handler(){
+
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what==111){
+                startActivity(new Intent(SplashActivity.this,StartActivity.class));
+                timer.cancel();
+            }
+        }
+    };
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.sendMessage(handler.obtainMessage(111));
+            }
+        }, 500, 2000);
+
+
+
     }
 }
