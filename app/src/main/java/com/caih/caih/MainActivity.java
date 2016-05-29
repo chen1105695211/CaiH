2ff269c9bc5457bad2855eaca1ab52f591fcd691
jyj123456789 <986753566@qq.com>
2016-05-29 21:09:31 +0800

    Update MainActivity.java

--- app/src/main/java/com/caih/caih/MainActivity.java
+++ app/src/main/java/com/caih/caih/MainActivity.java
@@ -26,14 +26,11 @@ public class MainActivity extends FragmentActivity  implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        manager= getSupportFragmentManager();
+        manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
-        transaction.add(R.id.main_frag, new Kczx_frag()).hide(new Chtk_Frag()).hide(new Grzx_Frag()).hide(new Xxzx_Frag());
-
-
+        transaction.add(R.id.main_frag, new Kczx_frag());
 
         findId();
-        kczx.setBackgroundResource(R.drawable.kechengzhongxin_select);
 
         transaction.commit();
     }
@@ -43,42 +40,34 @@ public class MainActivity extends FragmentActivity  implements View.OnClickListe
         RadioButton chtk = (RadioButton) findViewById(R.id.chtk);
         RadioButton xxzx = (RadioButton) findViewById(R.id.xxzx);
         RadioButton grzx = (RadioButton) findViewById(R.id.grzx);
-
         //监听
         kczx.setOnClickListener(this);
         chtk.setOnClickListener(this);
         xxzx.setOnClickListener(this);
         grzx.setOnClickListener(this);
 
-
     }
-
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
 
             case R.id.kczx:
                 FragmentTransaction transaction1 = manager.beginTransaction();
-                transaction1.add(R.id.main_frag, new Kczx_frag()).hide(new Chtk_Frag()).hide(new Grzx_Frag()).hide(new Xxzx_Frag()).commit();
-                kczx.setBackgroundResource(R.drawable.bt_caihua_selector);
+                transaction1.replace(R.id.main_frag, new Kczx_frag()).commit();
                 break;
             case R.id.chtk:
                 FragmentTransaction transaction2 = manager.beginTransaction();
-                transaction2.add(R.id.main_frag, new Chtk_Frag()).hide(new Kczx_frag()).hide(new Grzx_Frag()).hide(new Xxzx_Frag()).commit();
-                kczx.setBackgroundResource(R.drawable.bt_caihua_selector);
-                kczx.setBackgroundResource(R.drawable.bt_caihua_selector);
+                transaction2.replace(R.id.main_frag, new Chtk_Frag()).commit();
                 break;
             case R.id.xxzx:
                 FragmentTransaction transaction3 = manager.beginTransaction();
-                transaction3.add(R.id.main_frag, new Xxzx_Frag()).hide(new Chtk_Frag()).hide(new Grzx_Frag()).hide(new Kczx_frag()).commit();
+                transaction3.replace(R.id.main_frag, new Xxzx_Frag()).commit();
                 break;
             case R.id.grzx:
                 FragmentTransaction transaction4 = manager.beginTransaction();
-                transaction4.add(R.id.main_frag, new Grzx_Frag()).hide(new Chtk_Frag()).hide(new Kczx_frag()).hide(new Xxzx_Frag()).commit();
-                kczx.setBackgroundResource(R.drawable.bt_caihua_selector);
+                transaction4.replace(R.id.main_frag, new Grzx_Frag()).commit();
                 break;
-
 
 
         }
